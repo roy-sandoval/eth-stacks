920d4c8ad822d4e1b41f6c2406787b907bd01dfb
rpm <ryan.macarthur@gmail.com>
2023-06-24 16:51:29 -0400

    browser extension impl

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
@@ -14,10 +17,6 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 };
 
 export default config;
