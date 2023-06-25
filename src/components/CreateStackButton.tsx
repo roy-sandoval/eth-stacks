8ea836b11ae26c0e3d52c01792d1cdec29d75975
roy-sandoval <roy@roysandoval.com>
2023-06-25 08:17:13 -0400

    Mint Button Fix

--- src/components/CreateStackButton.tsx
+++ src/components/CreateStackButton.tsx
@@ -1,53 +1,51 @@
-import { useState } from 'react';
-import { BaseError } from 'viem';
+import { useState } from "react";
+import { BaseError } from "viem";
 import {
   useAccount,
   useContractWrite,
   usePrepareContractWrite,
   useWaitForTransaction,
-} from 'wagmi'
-import { useDebounce } from './useDebounce'
+} from "wagmi";
+import { useDebounce } from "./useDebounce";
 //import activeChain from '~/utils/activeChain'
-import React from 'react';
-import abiPath from '../utils/deployments/goerli/FinanceNFTFactory.json';
-import addresses from  '../utils/deployments/goerli/addresses.json';
+import React from "react";
+import abiPath from "../utils/deployments/goerli/FinanceNFTFactory.json";
+import addresses from "../utils/deployments/goerli/addresses.json";
 
 export function CreateStackButton() {
-  const [tokenId, setTokenId] = React.useState('')
-  const debouncedTokenId = useDebounce(tokenId)
-
-
+  const [tokenId, setTokenId] = React.useState("");
+  const debouncedTokenId = useDebounce(tokenId);
 
   //const [tokenId, setTokenId] = useState('')
   //const debouncedTokenId = useDebounce(tokenId)
   const { address } = useAccount();
   const { config, error } = usePrepareContractWrite({
-    address: `0x${addresses.factoryAddress}`,
+    address: addresses.factoryAddress,
     abi: abiPath.abi,
-    functionName: 'createFinanceNFT',
-    args: ["ETH Stacks NFT", "STACK", address, addresses.uri , addresses.registryAddress, addresses.tbaImplementationAddress ]
+    functionName: "createFinanceNFT",
+    args: [
+      "ETH Stacks NFT",
+      "STACK",
+      address,
+      addresses.uri,
+      addresses.registryAddress,
+      addresses.tbaImplementationAddress,
+    ],
   });
-  const {data, write} = useContractWrite(config)
+  const { data, write } = useContractWrite(config);
   const { isLoading, isSuccess } = useWaitForTransaction({
     hash: data?.hash,
-  })
+  });
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-        write?.()
-      }}
-    >
-      <label for="tokenId">Token ID</label>
-      <input
-        id="tokenId"
-        onChange={(e) => setTokenId(e.target.value)}
-        placeholder="420"
-        value={tokenId}
-      />
-      <button  className="w-full rounded-full bg-black px-12 py-4 text-white" disabled={!write || isLoading}>
-        {isLoading ? 'Minting...' : 'Mint'}
+    <div>
+      <button
+        className="w-full rounded-full bg-black px-12 py-4 text-white"
+        disabled={!write || isLoading}
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises, @typescript-eslint/require-await
+        onClick={async () => write?.()}
+      >
+        {isLoading ? "Minting..." : "Mint"}
       </button>
       {error && (
         <div>An error occurred preparing the transaction: {error.message}</div>
@@ -56,10 +54,12 @@ export function CreateStackButton() {
         <div>
           Successfully minted your NFT!
           <div>
-            <a href={`https://etherscan.io/tx/${data?.hash}`}>Etherscan</a>
+            <a href={`https://goerli.etherscan.io/tx/${data?.hash}`}>
+              Etherscan
+            </a>
           </div>
         </div>
       )}
-    </form>
-  )
+    </div>
+  );
 }
